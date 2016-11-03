739bb45c7374ff3142246975ee1bdd391c96487e
Nelson Crosby <nc@sourcecomb.com>
2016-11-03 18:45:31 +1300

    Rewrote sprite library
    Now slightly more succinct and documented

--- sprites.py
+++ sprites.py
@@ -1,117 +1,107 @@
-import io
 import pyglet.image
-import pyglet.resource
 
 
-class FrameData:
+class SpritesheetBase:
     @classmethod
-    def parse(cls, src, c):
-        pos, size, anim = src.split(' ')
-        posy, posx = tuple(map(int, pos.split(',')))
-        w, h = tuple(map(int, size.split('x')))
-        nf, rate = tuple(map(int, anim.split('@')))
-
-        framelen = 1.0 / rate
-        return [FrameData(
-                ((posx+i) * c, posy * c,
-                    w*c - 1, h*c - 1),
-                framelen
-            ) for i in range(nf)]
-
-    def __init__(self, bounds, length):
-        self.bounds = bounds
-        self.length = length
-
-
-class Layout:
-    def __init__(self, toparse, *, _cellsize=None, _sprites=None):
-        if toparse is None:
-            self._cellsize = _cellsize
-            self.sprites = _sprites
-        elif isinstance(toparse, str):
-            self._parse_str(toparse)
-        elif isinstance(toparse, io.IOBase):
-            self._parse_str(toparse.read().decode('utf-8'))
-        else:
-            raise TypeError('invalid layout type: ' + repr(toparse))
-
-    def _parse_str(self, src):
+    def _parse(cls, src):
         lines = src.split('\n')
+        # Ensure we have a valid source
         header = lines.pop(0)
         if not header.startswith('spritesheet;'):
-            raise ValueError('invalid layout header: {!r}'.format(header))
+            raise ValueError('Invalid spritesheet header: {!r}'
+                    .format(header))
+        # Get the cell width
         header = header.split(';')[1:]
-        self._cellsize = int(header[0])
+        c = int(header[0])
 
-        sprites = {}
+        # Parse key/value section
+        sprites = []
         for line in lines:
-            if not line:
-                continue
-            key, value = line.split('=')
-            key = key.strip()
-            value = value.strip()
+            if line:
+                # Separate key and value
+                key, value = line.split('=')
+                key = key.strip()
+                # Parse value section into parts
+                pos, size, anim = value.strip().split(' ')
+                y, x = map(int, pos.split(','))
+                w, h = map(int, size.split('x'))
+                nframes, rate = anim.split('@')
+                nframes = int(nframes)
+                rate = float(rate)
+                # Generate frame info
+                fdur = 1 / rate
+                value = [( ((x+i) * c, y * c, w*c - 1, h*c - 1), fdur )
+                        for i in range(nframes)]
+                # Add this frameset to list of sprites
+                sprites.append((key, value))
+        return sprites
 
-            ks = key.split('.')
-            elem = sprites
-            for k in ks[:-1]:
-                if k.isnumeric():
-                    k = int(k)
-                if k not in elem:
-                    elem[k] = {}
-                elem = elem[k]
-
-            elem[ks[-1]] = FrameData.parse(value, self._cellsize)
+    def __init__(self, sprites, canvas):
+        # Ensure any sprite sources are parsed
+        if isinstance(sprites, str):
+            sprites = SpritesheetBase._parse(sprites)
+        # Convert list of sprites to dict
+        if isinstance(sprites, list):
+            sd = {}
+            for sname, sinfo in sprites:
+                skeys = sname.split('.')
+                elem = sd
+                for key in skeys[:-1]:
+                    if key not in elem:
+                        elem[key] = {}
+                    elem = elem[key]
+                elem[skeys[-1]] = sinfo
+            sprites = sd
+        # Set properties
         self.sprites = sprites
+        self.canvas = canvas
 
     def __getitem__(self, key):
-        ks = key.split('.', 1)
-        info = self.sprites[ks[0]]
-        if isinstance(info, dict):
-            info = Layout(
-                    None, _cellsize=self._cellsize, _sprites=info
-            )
-            self.sprites[ks[0]] = info
-        if isinstance(info, Layout) and len(ks) > 1:
-            return info[ks[1]]
+        # Get key from nested dict
+        elem = self.sprites
+        for k in key.split('.'):
+            elem = elem[k]
+        # Convert info to an Animation and return
+        return pyglet.image.Animation(
+            [pyglet.image.AnimationFrame(
+                self.canvas.get_region(*frame[0]), frame[1]
+            ) for frame in elem]
+        )
+
+
+class Spritesheet(SpritesheetBase):
+    @classmethod
+    def open(cls, name, canvas=None):
+        if name.endswith('.spr'):
+            # Using explicit file names
+            srcf = name
+            if canvas is None:
+                canvas = name[:-4] + '.png'
         else:
-            return info
+            # Using template file names
+            srcf = name + '.spr'
+            canvas = name + '.png'
+        # Read data from files
+        with pyglet.resource.file(srcf) as srcf:
+            src = srcf.read().decode('utf-8')
+        canvas = pyglet.resource.image(canvas)
+        # Return a Spritesheet instance
+        return Spritesheet(src, canvas)
 
-
-class SpriteSheet:
-    def __init__(self, layout, canvas):
-        if not isinstance(layout, Layout):
-            layout = Layout(layout)
-        self.layout = layout
-        self.canvas = canvas
-        self._cache = {}
+    def __init__(self, sprites, canvas):
+        super().__init__(sprites, canvas)
 
     def __getattr__(self, key):
-        return self[key]
-
-    def __getitem__(self, key):
+        # Check the cache
         if key in self._cache:
             return self._cache[key]
-
-        info = self.layout[key]
-        if isinstance(info, Layout):
-            val = SpriteSheet(info, self.canvas)
+        # Nothing in cache
+        if isinstance(self.sprites[key], dict):
+            # Not a final key; create nested spritesheet
+            val = Spritesheet(self.sprites[key], self.canvas)
         else:
-            frames = [
-                    pyglet.image.AnimationFrame(
-                        self.canvas.get_region(*framedata.bounds),
-                        framedata.length
-                    ) for framedata in info
-            ]
-            val = pyglet.image.Animation(frames)
-
+            # This must be a final key
+            val = self[key]
+        # Set this value in cache
         self._cache[key] = val
         return val
-
-
-def load(name, canvas=None):
-    if canvas is not None:
-        src = name
-    else:
-        src = pyglet.resource.file(name + '.spr')
-        canvas = pyglet.resource.image(name + '.png')
-    return SpriteSheet(src, canvas)
