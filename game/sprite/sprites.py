5ca947d6b8d9c07db206d4f33b7fbce4c9def054
Nelson Crosby <nc@sourcecomb.com>
2016-11-17 14:00:45 +1300

    Revert "Added sprite-switching behaviour when a Spritable Entity switches states."
    This reverts commit 4f67fa8f82a4483d53095e6eb2b5b0dadb2c8b08.
    
    I was going to refactor this, but I'm thinking a full rewrite
    might be easier.

--- game/sprite/sprites.py
+++ game/sprite/sprites.py
@@ -6,19 +6,8 @@ class PlayerSheet(Spritesheet):
 
 
 class PlayerSprite(PlayerSheet):
-    right_idle = Sprite(
-        pos=(256, 480), size=(32, 32), frames=2, scale=2, rate=1
-    )
-    left_idle = Sprite(
-        pos=(320, 480), size=(32, 32), frames=2, scale=2, rate=1
-    )
-
-    right_run = Sprite(
-        pos=(256, 448), size=(32, 32), frames=8, scale=2, rate=10
-    )
-    left_run = Sprite(
-        pos=(256, 416), size=(32, 32), frames=8, scale=2, rate=10
-    )
+    idle = Sprite(pos=(256, 480), size=(32, 32), frames=2, scale=2, rate=1)
+    run = Sprite(pos=(256, 448), size=(32, 32), frames=8, scale=4, rate=10)
 
 
 
